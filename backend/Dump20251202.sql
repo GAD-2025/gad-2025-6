4a69341a25f182913dad5863121ab17f8c2bb750
sswu-jay <jay@sungshin.ac.kr>
2025-12-02 15:21:32 +0900

    수정: dump data는 없고 structure 만 업로드 해야함

--- backend/Dump20251202.sql
+++ backend/Dump20251202.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 9.3.0, for macos15.2 (arm64)
 --
--- Host: 127.0.0.1    Database: todak
+-- Host: localhost    Database: todak
 -- ------------------------------------------------------
--- Server version	8.0.44
+-- Server version	9.3.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -38,16 +38,6 @@ CREATE TABLE `bucket_list` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bucket_list`
---
-
-LOCK TABLES `bucket_list` WRITE;
-/*!40000 ALTER TABLE `bucket_list` DISABLE KEYS */;
-INSERT INTO `bucket_list` VALUES (1,1,NULL,'집 가자',0,'2025-12-01 15:45:50'),(2,1,NULL,'내년 크리스마스는 파리에서!',0,'2025-12-01 15:46:25');
-/*!40000 ALTER TABLE `bucket_list` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `dday`
 --
 
@@ -66,16 +56,6 @@ CREATE TABLE `dday` (
   CONSTRAINT `dday_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `dday`
---
-
-LOCK TABLES `dday` WRITE;
-/*!40000 ALTER TABLE `dday` DISABLE KEYS */;
-INSERT INTO `dday` VALUES (1,1,'집','2025-12-25','2025-12-01 15:17:33','집으로 ');
-/*!40000 ALTER TABLE `dday` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `quizzes`
@@ -97,16 +77,6 @@ CREATE TABLE `quizzes` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `quizzes`
---
-
-LOCK TABLES `quizzes` WRITE;
-/*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
-INSERT INTO `quizzes` VALUES (1,1,'ㅎㅇㄹ','ㅎㅇ','2025-11-28 14:13:08');
-/*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `slow_letters`
 --
 
@@ -121,18 +91,8 @@ CREATE TABLE `slow_letters` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `slow_letters_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `slow_letters`
---
-
-LOCK TABLES `slow_letters` WRITE;
-/*!40000 ALTER TABLE `slow_letters` DISABLE KEYS */;
-INSERT INTO `slow_letters` VALUES (1,1,'하이루','2025-11-28 13:58:55');
-/*!40000 ALTER TABLE `slow_letters` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -151,18 +111,8 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `user_code` (`user_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Yejin','dana020130@naver.com','$2b$10$lR73cuNfvh40cadJBbXbbeVWpLqb4xsUOUh1beo0psgcpTKfE8HWK','2025-11-28 12:53:40','4hCWQnnh');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -173,4 +123,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-02  0:55:03
+-- Dump completed on 2025-12-02 15:15:17
