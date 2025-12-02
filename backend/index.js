1128594916cc2479adb3b98d08590bea8b75152f
sswu-jay <jay@sungshin.ac.kr>
2025-12-02 15:21:46 +0900

    추가: morgan (backend logging)

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3001;
@@ -12,6 +13,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+app.use(morgan('dev'));
 
 // MySQL Connection Pool 설정
 const pool = mysql.createPool({
